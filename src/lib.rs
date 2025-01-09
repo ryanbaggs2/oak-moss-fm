f68efcd0e5a05c1728695370fcaa5199201fc512
Ryan Baggs <ryanbaggs7@gmail.com>
2025-01-08 21:04:55 -0600

    Add entry_names_sorted and working_dir_entry_names_sorted fns
    Signed-off-by: Ryan Baggs <ryanbaggs7@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! A simple file manager.
+
+use std::{fs, io};
+use std::ffi::{OsString};
+
+pub fn working_dir_entry_names_sorted() -> Result<Vec<OsString>, io::Error> {
+    entry_names_sorted(".")
+}
+
+pub fn entry_names_sorted(path: &str) -> Result<Vec<OsString>, io::Error> {
+    let mut entry_names = fs::read_dir(path)?
+        .map(|result| result.map(|entry| entry.file_name()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    entry_names.sort();
+
+    Ok(entry_names)
 }
 
 #[cfg(test)]
@@ -8,7 +23,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
     }
 }
